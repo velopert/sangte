50349415034013aa2bac9f3e298b30fe110e5adf
velopert <public.velopert@gmail.com>
2022-09-12 02:39:03 +0900

    fix: remove atom

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,3 @@
-export * from './atom'
+export * from './sangte'
 export * from './SangteInitializer'
 export * from './SangteManager'
-export { atom as sangte } from './atom' // for compatibility with old version
